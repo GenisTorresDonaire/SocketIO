751b8c24b8760e66e42178975fe93bcd8d4ee248
gtd97 <genistorres97@gmail.com>
2018-04-24 17:14:26 +0200

    finalizado falta estetica

--- index.js
+++ index.js
@@ -16,35 +16,48 @@ http.listen(port, function(){
 
 
 io.on('connection', function(socket){
-  //usuarios++;
-  //console.log("Usuarios actuales: " + usuarios);
-
   // Función que se encarga de mandar los nuevos mensajes
   socket.on('new message', function(msg){
-    io.emit('new message', msg);
+    // Preparando el json de envio, para los nuevos mensajes, donde se muestra el usuario y el mensaje que ha enviado.
+    io.emit('new message', {
+      usuario: socket.usuario,
+      mensaje: msg
+    });
   });
 
   // Funcion que se encarga de gestionar los nuevos usuarios
   socket.on('new user', function(data, callback){
-  	test = datosUsuarios.indexOf(data);
-  	if (test != -1){
+  	// Aqui miramos si el nickname que estan enviando no esta ya conectado actualmente.
+    respuesta = datosUsuarios.indexOf(data);
+
+    // Si retorna -1 no hay ninguno con ese nombre, asi que entrara en el else, sino existe y entra en el if.
+    if (respuesta != -1){
+      // devuelve un false, para que sepa que ese username ya esta ocupado.
   		callback(false);
+    // en el caso de que no haya sido encontrado en el array.
   	}else{
+      // devuelve un true
   		callback(true);
-  		socket.usuario = data;
+  		// Obtiene el nickname y lo guarda global.
+      socket.usuario = data;
+      // Hace un push en el array para que conste que ya existe.
   		datosUsuarios.push(socket.usuario);
+      // Llamada para actualizar el listado, y para crear el nuevo usuario.
   		io.emit("listado usuarios", datosUsuarios);
+      io.emit("nuevo usuario", socket.usuario);
   	}
   });
 
-  // Funcion de desconectarse
+  // Funcion que recibe los sockets desconectado para enviarlos al cliente y que informe por el chat.
   socket.on('disconnect', function(){
+    // en caso de que no sea el desconectado, no hara nada.
   	if(!socket.usuario) return;
   	usuarios--;
+    // aqui se borra el usuario desconectado.
   	datosUsuarios.splice(datosUsuarios.indexOf(socket.usuario), 1);
+    // llamada a dos funciones, una para actualizar la lista una vez se haya ido alguien. Y la segunda que se encarga de enviar el usuario desconectado al cliente.
   	io.emit("listado usuarios", datosUsuarios);
-
-  	console.log("Usuario desconectado.");
+    io.emit("usuario desconectado", socket.usuario);
   });
 
 });
